da28b5f61d884fbc0a8ea0cd7431acf5c167397b
EugeneGarbuzov <ridaim@yandex.ru>
2018-12-31 16:15:04 +0300

    Features generation added.

--- script.js
+++ script.js
@@ -7,29 +7,48 @@ const professions = [
 ];
 
 const genderAge = [
-
+    'М. 35-40',
+    'М. 15-20',
+    'М. 55-60',
 ];
 
 const qualities = [
-
+    'Агрессивный',
+    'Активный',
+    'Аморальный',
 ];
 
 const hobbies = [
-
+    'Боулинг',
+    'Вязание',
+    'Кулинария',
 ];
 
 const health = [
-
+    'Очень плохое зрение, но есть очки',
+    'Провалы в памяти',
+    'Возможное заражение вирусом Эбола',
 ];
 
 const additionals = [
-
+    'Знает семь языков',
+    'Не умеет плавать',
+    'Большая тяга к алкоголю',
 ];
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-document.getElementById('profession').textContent = professions[getRandomInt(0, professions.length - 1)];
+function generateFeature(elementId, values) {
+    document.getElementById(elementId).textContent = values[getRandomInt(0, values.length - 1)];
+}
+
+generateFeature('profession', professions);
+generateFeature('genderAge', genderAge);
+generateFeature('quality', qualities);
+generateFeature('hobby', hobbies);
+generateFeature('health', health);
+generateFeature('additional', additionals);
 
 
